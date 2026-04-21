10fabcbb45867cd408a86260b3e3927feea92151
A1exVd <vdovichenko99a@gmail.com>
2026-04-21 18:09:05 +0300

    fixed

--- src/generator.js
+++ src/generator.js
@@ -1,18 +1,35 @@
 import { faker } from '@faker-js/faker';
 import fs from 'fs/promises';
 import chalk from 'chalk';
+import { nanoid } from 'nanoid';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 /* 
 Использую type: module, так как является современным стандартом,
-позволяет использовать await без обертывания его async функцией,модули всегда выполняются в строгом режиме.
+позволяет использовать top-level await, модули всегда выполняются в строгом режиме.
 ESM использует статический синтаксис import/export, что позволяет инструментам
 сборки эффективнее удалять неиспользуемый код (tree-shaking).
+*/
+
+/* 
+Использую fs/promises, так как, в отличие от fs, позволяет писать асинхронный код, используя промисы, а не колбеки.
+Удобнее обрабатывать ошибки, используя try/catch конструкцию. Не блокирует основной поток, позволяет выполнять несколько 
+операций параллельно и ждать их завершения. (Promise.all())
+*/
+
+/* 
+Зачем React нужны keys?
+При рендеринге списков, например, карточек товара, для каждого элемента списка необходимо добавить уникальный ключ идентификатор.
+Это поможет оптимизировать рендеринг списков. При именении состояния ключи помогают алгоритмам сравнения нового виртуального DOM со 
+старым, в частности определить какой элемент обновился и вместо обновления всего списка обновить только изменившийся элемент. 
+Тем самым улучшив производительность страницы.
 */
 
 
 export function createRandomUser() {
   return {
-    userId: faker.string.uuid(),
+    userId: nanoid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
     avatar: faker.image.avatar(),
@@ -27,15 +44,30 @@ export const users = faker.helpers.multiple(createRandomUser, {
 });
 
 
+// Путь к файлу db.json 
+
+/*
+Использую абсолютный путь, полученный с помощью path, а не относительный, так как абсолютный 
+всегда ведет к файлу независимо от того, откуда вызвывается скрипт. Более недежен и удобен при изменении структуры проекта.
+*/
+
+const currentfilePath = fileURLToPath(import.meta.url);
+// ПОЧЕМУ? Использую dirname для получения пути к родительской папке.
+const currentDir = path.dirname(currentfilePath);
+const rootDir = path.dirname(currentDir);
+// ПОЧЕМУ? Использую join для объединения нескольких частей пути в одну строку.
+const pathDbFile = path.join(rootDir, 'data', 'db.json');
+
 async function writeToFile(data, filePath) {
     try {
+        // ПОЧЕМУ? асинхронный метод, используется для записи данных в файл. Если файла нет - создает его и записывает данные.
         await fs.writeFile(filePath, data) 
     } catch (error) {
-        console.log('Some error: ', error);
-        return;
+        console.error('Some error: ', error);
+        process.exit(1);
     }
     console.log(chalk.green('db.json создна успешно!'));
 }
 
 
-writeToFile(JSON.stringify(users), 'data/db.json');
+writeToFile(JSON.stringify(users), pathDbFile);
--- src/validator.js
+++ src/validator.js
@@ -1,13 +1,63 @@
 import fs from 'fs/promises';
 import chalk from 'chalk';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-async function checkStructure(path) {
+
+// Путь к файлу db.json
+const currentfilePath = fileURLToPath(import.meta.url);
+// ПОЧЕМУ? Использую dirname для получения пути к родительской папке.
+const currentDir = path.dirname(currentfilePath);
+const rootDir = path.dirname(currentDir);
+// ПОЧЕМУ? Использую join для объединение нескольких частей пути в одну строку.
+const pathDbFile = path.join(rootDir, 'data', 'db.json');
+
+// Проверка структуры репозитория
+checkStructure(path.join(rootDir,'src'));
+checkStructure(path.join(rootDir,'data'));
+
+// Валидация db.json файла
+validate();
+
+async function validate() {
     try {
-        await fs.access(path);
+        const data = await fs.readFile(pathDbFile, 'utf8');
+        
+        const dataArr = JSON.parse(data);
+        
+        if(!dataArr) {
+            throw new Error(chalk.red('dataArr возможно null или undefined!'))
+        }
+
+        if(!Array.isArray(dataArr)) {
+            throw new Error(chalk.red('dataArr должен быть массивом!'));
+        }
+        // Простая проверка наличия всех свойств у пользователя
+        dataArr.forEach((user) => {
+            if(!user.userId) throw new Error(chalk.red('userId возможно null или undefined!'));
+            else if (!user.username) throw new Error(chalk.red('username возможно null или undefined!'));
+            else if (!user.email) throw new Error(chalk.red('email возможно null или undefined!'));
+            else if (!user.avatar) throw new Error(chalk.red('avatar возможно null или undefined!'));
+            else if (!user.password) throw new Error(chalk.red('password возможно null или undefined!'));
+            else if (!user.birthdate) throw new Error(chalk.red('birthdate возможно null или undefined!'));
+            else if (!user.registeredAt) throw new Error(chalk.red('registeredAt возможно null или undefined!'));
+        })
+
+        console.log(chalk.green('Успешная валидация структуры проекта!'));
     } catch (error) {
-        console.log(chalk.red(`Ошибка структуры ${path} не найден!`));
+        console.error(error)
+        // прекращаем работу программы
+        process.exit(1);
     }
 }
 
-checkStructure('src');
-checkStructure('data');
+async function checkStructure(path) {
+    try {
+        // ПОЧЕМУ? метод access проверяет существование файла/папки, а такжеправа доступа пользователя к файлу/папке.
+        await fs.access(path);
+    } catch (error) {
+        console.error(chalk.red(`Ошибка структуры ${path} не найден!`));
+        // прекращаем работу программы
+        process.exit(1);
+    }
+}
